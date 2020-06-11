8293ba64f0fc6af91ed5b37525e2bae1f65202cc
SamuelMwangi-YR <63617455+SamuelMwangi-YR@users.noreply.github.com>
2020-06-11 09:50:16 -0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,12 @@
+<?php
+if(){//show data
+
+}else{//show form
+  echo " (single or double quotes works as long as is consistent)
+  <form action="">(goes back to the previous html)
+  
+  </form>
+  
+  ":
+}
+?>
